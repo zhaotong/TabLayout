cfea079939ebe019397ffa578f9e53b73f1c6c6f
Tone <372375692@qq.com>
2019-04-23 17:28:58 +0800

    Initial commit

---
+++ app/src/main/java/com/tablayout/MainActivity.java
@@ -0,0 +1,118 @@
+package com.tablayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] channels = new String[]{"GINGERBREAD", "HONEYCOMB", "ICE_CREAM_SANDWICH", "JELLY_BEAN", "KITKAT", "LOLLIPOP", "M", "NOUGAT"};
+
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return channels.length;
+            }
+
+            @NonNull
+            @Override
+            public Object instantiateItem(@NonNull ViewGroup container, int position) {
+                final TextView textView = new TextView(container.getContext());
+                textView.setText(channels[position]);
+                textView.setGravity(Gravity.CENTER);
+                textView.setTextSize(24);
+                container.addView(textView);
+                return textView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return channels[position];
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+                return view == o;
+            }
+        });
+
+        tabLayout.setOnCreateTabCustomViewListener(new TabLayout.OnCreateTabCustomViewListener() {
+            @Override
+            public View OnCreateTabView(TabLayout.TabView view, int position) {
+                View view1 = LayoutInflater.from(view.getContext()).inflate(R.layout.tab_item_icon_layout, view, false);
+                TextView textView = view1.findViewById(R.id.tab_txt);
+                textView.setText(channels[position]);
+                textView.setTextColor(Color.BLACK);
+                ImageView imageView = view1.findViewById(R.id.tab_icon);
+                if (position % 4 == 0)
+                    imageView.setVisibility(View.VISIBLE);
+                imageView.setImageResource(R.mipmap.ic_tab_selected);
+
+                return view1;
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                TextView textView = tab.view.findViewById(R.id.tab_txt);
+                textView.setText(channels[tab.getPosition()]);
+                textView.setTextColor(Color.WHITE);
+                ImageView imageView = tab.view.findViewById(R.id.tab_icon);
+                imageView.setImageResource(R.mipmap.ic_tab_selected);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                TextView textView = tab.view.findViewById(R.id.tab_txt);
+                textView.setText(channels[tab.getPosition()]);
+                textView.setTextColor(Color.BLACK);
+                ImageView imageView = tab.view.findViewById(R.id.tab_icon);
+                imageView.setImageResource(R.mipmap.ic_tab_unselected);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        RoundShadowIndicator roundIndicator = new RoundShadowIndicator();
+        roundIndicator.setRoundRadius(200);
+        roundIndicator.setUseGradient(true);
+        roundIndicator.setUseShadow(true);
+
+        tabLayout.setSelectedTabIndicator(roundIndicator);
+
+        tabLayout.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+//        tabLayout.setTabTextColors(Color.BLACK,Color.WHITE);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
---
+++ app/src/main/java/com/tablayout/RoundShadowIndicator.java
@@ -0,0 +1,90 @@
+package com.tablayout;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Author: tone
+ * Date: 2019-04-23 15:40
+ * Description:
+ */
+public class RoundShadowIndicator extends Drawable {
+
+    private Paint paint;
+    private float roundRadius;//圆角
+    private boolean useShadow = false;
+    private boolean useGradient = false;
+
+    private int[] colors;
+
+
+    public RoundShadowIndicator() {
+        paint = new Paint();
+        paint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        colors = new int[]{Color.GRAY, Color.DKGRAY};
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        Rect rect = getBounds();
+
+        if (useShadow) {
+            paint.setShadowLayer(30, 0, 15, Color.parseColor("#FFA35065"));
+        }
+        if (useGradient && colors.length > 1) {
+            Shader shader = new LinearGradient(rect.left, 0, rect.right, 0, colors, null, Shader.TileMode.CLAMP);
+            paint.setShader(shader);
+        } else {
+            paint.setColor(colors[0]);
+        }
+        if (roundRadius > 0) {
+            canvas.drawRoundRect(new RectF(rect), roundRadius, roundRadius, paint);
+        } else {
+            canvas.drawRect(new RectF(rect), paint);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        paint.setAlpha(alpha);
+    }
+
+    public void setRoundRadius(float roundRadius) {
+        this.roundRadius = roundRadius;
+    }
+
+    public void setUseGradient(boolean useGradient) {
+        this.useGradient = useGradient;
+    }
+
+    public void setUseShadow(boolean useShadow) {
+        this.useShadow = useShadow;
+    }
+
+    public void setColor(int[] colors) {
+        if (colors == null || colors.length == 0)
+            return;
+        this.colors = colors;
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+        paint.setColorFilter(colorFilter);
+    }
+
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
